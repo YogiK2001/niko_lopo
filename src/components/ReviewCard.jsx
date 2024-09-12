efed2ba39d2d564401c8b2721f7fae627d617455
YogiK2001 <kaustubh7718@gmail.com>
2024-09-13 00:52:15 +0530

    done

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,27 @@
+import { star } from "../assets/icons";
+
+const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
+  return (
+    <div className="flex justify-center items-center flex-col ">
+      <img
+        src={imgURL}
+        alt="customer"
+        className="rounded-full object-cover w-[120px] h-[120px]"
+      />
+      <p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
+      <div className="mt-3 flex justify-center items-center gap-2.5">
+        <img
+          src={star}
+          alt="rating"
+          className="w-[24px] h-[24px] object-contain m-0"
+        />
+        <p className="text-xl font-montserrat text-slate-gray">({rating})</p>
+      </div>
+      <h3 className="mt-4 text-bold font-palanquin text-coral-red text-2xl">
+        {customerName}
+      </h3>
+    </div>
+  );
+};
+
+export default ReviewCard;
